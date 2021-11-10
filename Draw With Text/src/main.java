a480a4bc20561333d23f33b67265176ed0fcdd51
KaiHikichi <90804924+KaiHikichi@users.noreply.github.com>
2021-11-10 15:32:37 -0800

    Draw with Text Assignment

---
+++ Draw With Text/src/main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println(" ___________");
+        System.out.println("/___________\\_____");
+        System.out.println("|\t0   0\t|");
+        System.out.println("|\t  > \t|");
+        System.out.println("|\t\\___/\t|");
+        System.out.println("\\___________/");
+
+
+    }
+}
